552275e7cda9eb75d463814aaabeca1d070f7fd7
main <unfazing@gmail.com>
2025-03-25 17:04:24 +0000

    add rust file structure

---
+++ src/RustConductorEvaluator.ts
@@ -0,0 +1,44 @@
+import { BasicEvaluator } from "conductor/dist/conductor/runner";
+import { IRunnerPlugin } from "conductor/dist/conductor/runner/types";
+import { CharStream, CommonTokenStream } from 'antlr4ng';
+import { RustEvaluatorVisitor} from "./RustEvaluator"
+import { RustLexer } from './parser/src/RustLexer.js';
+import { RustParser } from './parser/src/RustParser.js';
+
+export class RustConductorEvaluator extends BasicEvaluator {
+    private executionCount: number;
+    private visitor: RustEvaluatorVisitor;
+
+    constructor(conductor: IRunnerPlugin) {
+        super(conductor);
+        this.executionCount = 0;
+        this.visitor = new RustEvaluatorVisitor();
+    }
+
+    async evaluateChunk(chunk: string): Promise<void> {
+        this.executionCount++;
+        try {
+            // Create the lexer and parser
+            const inputStream = CharStream.fromString(chunk);
+            const lexer = new RustLexer(inputStream);
+            const tokenStream = new CommonTokenStream(lexer);
+            const parser = new RustParser(tokenStream);
+            
+            // Parse the input
+            const tree = parser.prog();
+            
+            // Evaluate the parsed tree
+            const result = this.visitor.visit(tree);
+            
+            // Send the result to the REPL
+            this.conductor.sendOutput(`Result of expression: ${result}`);
+        }  catch (error) {
+            // Handle errors and send them to the REPL
+            if (error instanceof Error) {
+                this.conductor.sendOutput(`Error: ${error.message}`);
+            } else {
+                this.conductor.sendOutput(`Error: ${String(error)}`);
+            }
+        }
+    }
+}
